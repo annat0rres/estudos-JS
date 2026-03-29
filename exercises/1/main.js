7a9566dc1e28197fa7a86f84c50dba8b8ec78d22
Anna Torres <anna.torres@escolar.ifrn.edu.br>
2026-03-28 22:06:02 -0300

    Exercícios
    1 - Função e documentGetElementById
    2 - If, Else if e else; parseInt

---
+++ exercises/1/main.js
@@ -0,0 +1,4 @@
+function Acender(){
+    var lampada = document.getElementById("lamp")
+    lampada.src = "http://www.w3schools.com/js/pic_bulbon.gif" //mudar apenas o atributo src do eleemnto html img 
+}
---
+++ exercises/2/main.js
@@ -0,0 +1,14 @@
+idade = prompt("Digite sua idade: ")
+idade = parseInt(idade) //converter string em inteiro
+
+if (idade >= 0 && idade < 15){
+    document.write('Criança')
+} else if (idade >= 15 && idade < 30){
+    document.write('Jovem')
+} else if (idade >= 30 && idade < 60){
+    document.write("Adulto")
+} else if (idade >= 60){
+    document.write('Idoso')
+} else{
+    alert("Digite uma idade válida")
+}
