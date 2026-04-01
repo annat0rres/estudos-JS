f625aacaad764c1145446303e6f54474d6bbd938
Anna Torres <anna.torres@escolar.ifrn.edu.br>
2026-04-01 18:29:39 -0300

    funções de apagar JS
    C e Del da calculadora estão funcionando agora
    % também, só não faz operações ainda

--- miniprojects/calculator/main.js
+++ miniprojects/calculator/main.js
@@ -9,6 +9,20 @@ function MostrarResultado(){
     document.getElementById("visual").innerHTML = display.join("");
 }
 
+
+function Apagar(){
+    if (display.length !== 0) {
+        display.pop()
+        document.getElementById("visual").innerHTML = display.join("");
+}
+}
+
+function ApagarTudo(){
+    if (display.length !== 0) {
+        document.getElementById("visual").innerHTML = '';
+}
+}
+
 let posOperador = ''
 let resultado = ''
 
@@ -17,14 +31,62 @@ function Calcular(){
         if ("+" == display[i]){
             posOperador = parseInt(i);
             var n = display.slice(0, posOperador);
-            var num1 = parseInt(n.join(""))
+            var num1 = parseFloat(n.join(""))
             var n2 = display.slice(posOperador + 1)
-            var num2 = parseInt(n2.join(""))
+            var num2 = parseFloat(n2.join(""))
             let resultado = num1 + num2
+            resultado = parseFloat(resultado.toFixed(2));
+            display = []
+            display.push(resultado.toString())
+            MostrarResultado();
+        } else if ("-" == display[i]) {
+            posOperador = parseInt(i);
+            var n = display.slice(0, posOperador);
+            var num1 = parseFloat(n.join(""))
+            var n2 = display.slice(posOperador + 1)
+            var num2 = parseFloat(n2.join(""))
+            let resultado = num1 - num2
+            resultado = parseFloat(resultado.toFixed(2));
             display = []
             display.push(resultado.toString())
             MostrarResultado();
             // console.log("o numero antes do op: " + num1 + " e " + "o numero dps é: " + num2)
+        } else if ("*" == display[i]){
+            posOperador = parseInt(i);
+            var n = display.slice(0, posOperador);
+            var num1 = parseFloat(n.join(""))
+            var n2 = display.slice(posOperador + 1)
+            var num2 = parseFloat(n2.join(""))
+            let resultado = num1 * num2
+            resultado = parseFloat(resultado.toFixed(2));
+            display = []
+            display.push(resultado.toString())
+            MostrarResultado();
+            // console.log("o numero antes do op: " + num1 + " e " + "o numero dps é: " + num2)
+        } else if ("/" == display[i]){
+            posOperador = parseInt(i);
+            var n = display.slice(0, posOperador);
+            var num1 = parseFloat(n.join(""))
+            var n2 = display.slice(posOperador + 1)
+            var num2 = parseFloat(n2.join(""))
+            let resultado = num1 / num2
+            resultado = parseFloat(resultado.toFixed(2));
+            display = []
+            display.push(resultado.toString())
+            MostrarResultado();
+            // console.log("o numero antes do op: " + num1 + " e " + "o numero dps é: " + num2)
+        }  else if ("%" == display[i]){
+            posOperador = parseInt(i);
+            var n = display.slice(0, posOperador);
+            var num1 = parseInt(n.join(""))
+            // var n2 = display.slice(posOperador + 1)
+            // var num2 = parseInt(n2.join(""))
+            let resultado = num1 / 100
+            resultado = parseFloat(resultado.toFixed(2));
+            display = []
+            display.push(resultado.toString())
+            MostrarResultado();
         }
+        
     }
 }
